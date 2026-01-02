db18bc0e9ee8ec796fd123ef54682eccc54cdf9b
Vijesh6837 <vijeshwaran6837@gmail.com>
2026-01-02 10:45:40 +0530

    Add files via upload

--- project_ecommerce.sql
+++ project_ecommerce.sql
@@ -11,13 +11,6 @@ describe shipping;
 describe fact_table;
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-select * from customers;
-select * from orders;
-select * from products;
-select * from payments;
-select * from shipping;
-select * from fact_table;
--- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Assigning Primary Key
 alter table customers add primary key (customer_id);
@@ -37,8 +30,7 @@ alter table fact_table add constraint foreign key fkey_fact_shipping(shipping_id
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 1 Update loyalty points for customers based on age.
-
+-- TASK 1
 select * from customers;
 
 Update customers set loyalty_points = 
@@ -50,9 +42,9 @@ end;
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 2 Get total order value per country and classify sales category.
+-- TASK 2
 
-select round(sum(order_value)) from orders;
+select sum(order_value) from orders;
 select c.country, round(sum(o.order_value),2) as total_order_value,
 if(sum(o.order_value) > 100000, "High",
 if (sum(o.order_value) between 50000 and 100000, "Medium", "Low")) as Sales_category
@@ -63,7 +55,7 @@ group by c.country;
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 3 Pivot total order quantity per payment method.
+-- TASK 3
 
 select c.country, 
 count(case when o.payment_method = "Credit card" then o.order_id end) as Credit_Card_qty,
@@ -76,15 +68,13 @@ group by country;
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 4 (Joins with Subquery) Find top 3 customers by total order value and Customer_id (using Rank).
+-- TASK 4
 
 -- Using Joins and subqueries
 select r.customer_id, round(r.total_order_value,2)as total_order_value, r.order_rank 
 from (select customer_id,  sum(order_value) as total_order_value, rank() over(order by sum(order_value) desc) as order_rank
 from orders o group by customer_id) as r 
 join customers c on c.customer_id = r.customer_id where r.order_rank<=3 order by order_rank;
-
-use project_ecommerce;
 
 -- Using Common Table Expression CTE
 with ranked_customers as ( 
@@ -94,7 +84,7 @@ group by c.customer_id) select * from ranked_customers where order_rank<=3 order
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 5 Find products that have been ordered more than the average quantity.
+-- TASK 5
 
 select  product_name, count(product_name) as Product_Quantity
 from products group by product_name 
@@ -102,7 +92,7 @@ having  Product_Quantity > (select count(product_name)/ count(distinct product_n
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 6 Get all orders for a specific customer using customer_id
+-- TASK 6
 
 delimiter //
 create procedure order_details(in customer_orders int) 
@@ -114,11 +104,9 @@ delimiter ;
  
 call order_details(4677);
 
-drop procedure order_details;
-
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 7 Return the total spending of a customer.
+-- TASK 7
 
 delimiter //
 create procedure customer_orders(in customer_id int,  out total_spending decimal(8,2)) 
@@ -133,11 +121,9 @@ delimiter ;
 call customer_orders(45,@total_spending);
 select @total_spending;
 
-drop procedure customer_orders;
-
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 8 Automatically set loyalty points to 0 if NULL on insert.
+-- TASK 8
 
 delimiter //
 create trigger loyalty_check before insert on customers for each row
@@ -150,16 +136,19 @@ end //
 delimiter ;
 
 insert into customers values 
-(5001, "John", "john@gmail.com", "Male", 45, "Korea", "Seoul", "2021-12-15","2025-12-15",null);
+(5001, "John", "john@gmail.com", "Male", 45, "Korea", "Seoul", "2021-12-15","2025-12-15",null),
+(5002, "Phil", "phil@gmail.com", "Male", 25, "Bahamas", "South Marioview", "2022-08-05","2025-12-15",0);
+
+delete from customers where customer_id = 5001;
+delete from customers where customer_id = 5002;
 
 select * from customers where customer_id = 5001;
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- TASK 9 After a new order is inserted, deduct quantity from product stock.
+-- TASK 9
 
 create table order_window (order_id int, customer_id int, product_id int,order_quantity int, order_date date, order_value int, order_status varchar(100));
-
 delimiter //
 create trigger inventory_change after insert on order_window for each row
 begin
@@ -175,20 +164,21 @@ end if;
 end //
 delimiter ;
 
+
 insert into products values 
 (110, "Once Product","Books",	"Ward-Forbes", 405.4,500,5),
 (120, "Once Product","Books",	"Ward-Forbes", 405.4,200,5);
 
 insert into order_window values 
 (5001,5001,110,200,"2025-10-08",879.22,'Placed'),
-(5002,5002,120,1000,"2025-10-08",879.22,'Placed');
+(5002,5002,110,100,"2025-10-08",879.22,'Placed');
 
 
-select * from order_window;
-select * from orders where order_id in (5001,5002);
 select * from products;
 
+select * from orders;
 
+select * from order_window;
 
 drop trigger inventory_change;
 drop table order_window;
